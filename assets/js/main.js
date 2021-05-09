549df264be4d44fb208e7c956ae0d86c6c1db4e5
Keidson Roby <76180887+keidsondesigner@users.noreply.github.com>
2021-05-08 22:41:48 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+    
+    inicia(){
+      this.cliqueBotoes();
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if (!conta) {
+          alert('Conta inválida');
+          return;
+        }
+
+        this.display.value = conta;
+      } catch (e) {
+        alert('Conta inválida');
+        return;
+      }
+    },
+    
+    clearDisplay() {
+      this.display.value = '';
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+
+    cliqueBotoes(){
+      document.addEventListener('click', (e) => {
+        const el = e.target;
+        
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if(el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if(el.classList.contains('btn-del')) {
+          this.apagaUm();
+        }
+
+        if(el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    btnParaDisplay(valor){
+      this.display.value += valor;
+    },
+
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
